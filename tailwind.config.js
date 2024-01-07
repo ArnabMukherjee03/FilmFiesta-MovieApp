140b7f65f5da72e58c89a50286879df517445513
Arnab Mukherjee <arnabmukherjee58260@gmail.com>
2024-01-08 01:51:40 +0530

    File Setup Complete

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      boxShadow: {
+        custom:
+          "rgba(0, 0, 0, 0.4) 0px 2px 4px, rgba(0, 0, 0, 0.3) 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -3px 0px inset", // Your custom box shadow values
+      },
+      fontFamily: {
+        heading: ['"Jomolhari"', "serif"],
+        para: ['"Josefin Slab"', "serif"],
+      },
+    },
+  },
+  plugins: [],
+}
